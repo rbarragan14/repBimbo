2f33afad16e461e08b3f2ffe61c869e8dea5f479
Christian Axel Schmidt Dick (Teradata) <christianaxel.schmidt@teradata.com>
2021-12-29 02:27:59 +0100

    fix: moved files to proper folders

--- model_definitions/55d524c7-657a-443f-bde5-69f137c207ec/model_modules/training.py
+++ model_definitions/55d524c7-657a-443f-bde5-69f137c207ec/model_modules/training.py
@@ -53,3 +53,5 @@ def train(data_conf, model_conf, **kwargs):
     skl_to_pmml(pipeline=model, col_names=feature_names, target_name=target_name, pmml_f_name="artifacts/output/model.pmml")
 
     print("Saved trained model")
+
+    # TODO: add stats recording
